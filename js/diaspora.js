c45b1d73d2e52cb901c17a05d3b71b88793a225b
mgodw <zcabma0@ucl.ac.uk>
2020-05-14 09:09:23 +0100

    finished the structure, added scroll to top js, need to add info

--- js/diaspora.js
+++ js/diaspora.js
@@ -5,3 +5,22 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active');
 });
+
+myButton = document.getElementById("myBtn"); //get the button
+
+window.onscroll = function() {scrollFunction()}; //when user scrolls down 20px from top, show button
+
+function scrollFunction () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+//when user clicks on the button, it'll scroll to the top of the document
+
+function topFunction() {
+    document.body.scrollTop = 0; //for safari
+    document.documentElement.scrollTop = 0; //for chrome, ie, firefox, opera
+}
