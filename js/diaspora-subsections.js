0a1ebaf6e6d18d8e479e1f1c0d046b4208fdb606
munakaghamelu <zcambma0@ucl.ac.uk>
2020-06-02 15:54:55 +0100

    trying to complete lupita

--- js/diaspora-subsections.js
+++ js/diaspora-subsections.js
@@ -2,25 +2,41 @@ let mainNav = document.getElementById('js-menu');
 
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
-navBarToggle.addEventListener('click', function () {
-    mainNav.classList.toggle('active');
+navBarToggle.addEventListener('click', function() {
+  mainNav.classList.toggle('active');
 });
 
 myButton = document.getElementById("myBtn"); //get the button
 
-window.onscroll = function() {scrollFunction()}; //when user scrolls down 20px from top, show button
+window.onscroll = function() {
+  scrollFunction()
+}; //when user scrolls down 20px from top, show button
 
-function scrollFunction () {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        myButton.style.display = "block";
-    } else {
-        myButton.style.display = "none";
-    }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
 }
 
 //when user clicks on the button, it'll scroll to the top of the document
 
 function topFunction() {
-    document.body.scrollTop = 0; //for safari
-    document.documentElement.scrollTop = 0; //for chrome, ie, firefox, opera
+  document.body.scrollTop = 0; //for safari
+  document.documentElement.scrollTop = 0; //for chrome, ie, firefox, opera
 }
+
+$(document).ready(function() {
+  $('.fancybox').fancybox({
+    padding: 0,
+    maxWidth: '100%',
+    maxHeight: '100%',
+    width: 560,
+    height: 315,
+    autoSize: true,
+    closeClick: true,
+    openEffect: 'elastic',
+    closeEffect: 'elastic'
+  });
+});
