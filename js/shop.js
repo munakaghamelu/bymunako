b45cb13bbcc6160c125991566d00a32fefb08b32
munakaghamelu <zcabma0@ucl.ac.uk>
2020-08-28 10:37:39 +0100

    added remove cart item

--- js/shop.js
+++ js/shop.js
@@ -1,3 +1,43 @@
+// Make remove buttons remove from our cart
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready () {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    console.log(removeCartItemButtons)
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
+
+function removeCartItem() {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+// Toggle + Top
+
 myButton = document.getElementById("myBtn"); //get the button
 
 window.onscroll = function() {scrollFunction()}; //when user scrolls down 20px from top, show button
